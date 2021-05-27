d91c27ea2662a806a5fa623f5fa16a2d001a2402
Leonardo Omaña <leotangram@gmail.com>
2021-05-27 16:56:10 -0500

    First commit

---
+++ src/auth/components/login-page/login-page.js
@@ -0,0 +1,42 @@
+import { Button, TextField } from '@material-ui/core'
+import { useState } from 'react'
+
+const LoginPage = () => {
+  const [emailValidationMessage, setEmailValidationMessage] = useState('')
+  const [passwordValidationMessage, setPasswordValidationMessage] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const { email, password } = e.target.elements
+
+    if (!email.value) {
+      setEmailValidationMessage('The email is required')
+    }
+
+    if (!password.value) {
+      setPasswordValidationMessage('The password is required')
+    }
+  }
+
+  return (
+    <>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label="email"
+          id="email"
+          helperText={emailValidationMessage}
+        />
+        <TextField
+          label="password"
+          id="password"
+          type="password"
+          helperText={passwordValidationMessage}
+        />
+        <Button type="submit">Send</Button>
+      </form>
+    </>
+  )
+}
+
+export default LoginPage
---
+++ src/auth/components/login-page/login-page.test.js
@@ -0,0 +1,44 @@
+import { screen, render, fireEvent } from '@testing-library/react'
+import LoginPage from './login-page'
+
+beforeEach(() => render(<LoginPage />))
+
+describe('when login page is mounted', () => {
+  it('must display the login title', () => {
+    expect(screen.getByText(/login page/i)).toBeInTheDocument()
+  })
+
+  it('must have a form with the following fields: email, password and a submit button', () => {
+    expect(screen.getByLabelText(/email/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /send/i }))
+  })
+})
+
+describe('when the user leaves empty fields and clicks the submit button', () => {
+  it('display required messages as the format: "The [field name] is required"', () => {
+    expect(screen.queryByText(/the email is required/i)).not.toBeInTheDocument()
+    expect(
+      screen.queryByText(/the password is required/i),
+    ).not.toBeInTheDocument()
+
+    fireEvent.click(screen.getByRole('button', { name: /send/i }))
+
+    expect(screen.getByText(/the email is required/i)).toBeInTheDocument()
+    expect(screen.getByText(/the password is required/i)).toBeInTheDocument()
+  })
+})
+
+describe('when the user fills the fields and clicks the submit button', () => {
+  it('must not display the required messages', () => {
+    screen.getByLabelText(/email/i).value = 'john.doe@test.com'
+    screen.getByLabelText(/password/i).value = 'Aa123456789!@#'
+
+    fireEvent.click(screen.getByRole('button', { name: /send/i }))
+
+    expect(screen.queryByText(/the email is required/i)).not.toBeInTheDocument()
+    expect(
+      screen.queryByText(/the password is required/i),
+    ).not.toBeInTheDocument()
+  })
+})
